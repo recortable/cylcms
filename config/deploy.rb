0c4d64e481e8eee7221bbf042135addaa7b1c13f
danigb <danigb@gmail.com>
2010-10-04 22:54:27 +0200

    deploy with shared configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ require 'yaml'
 GIT = YAML.load_file("#{File.dirname(__FILE__)}/git.yml")
 
 default_run_options[:pty] = true
-set :application, "laogen"
+set :application, "cylcms"
 set :deploy_to, "/home/deploy/#{application}"
 set :user, "deploy"
 set :use_sudo, false
@@ -18,10 +18,24 @@ role :app, "toami.net"
 role :web, "toami.net"
 role :db,  "toami.net", :primary => true
 
+after "deploy:update_code", "config:copy_shared_configurations"
+after "deploy", "deploy:cleanup"
+
 namespace :deploy do
-  task :start {}
-  task :stop {}
+  task :start do
+  end
+  task :stop do
+  end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+namespace :config do
+  desc "copy shared configurations to current"
+  task :copy_shared_configurations, :roles => [:app] do
+    %w[database.yml pusher.yml].each do |f|
+      run "ln -nsf #{shared_path}/config/#{f} #{release_path}/config/#{f}"
+    end
   end
 end
